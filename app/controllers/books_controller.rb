370e63c49c3290a6f23ed3123638822f96f0f448
tangvinhduong <me@yeuem1vannam.com>
2018-10-01 18:10:24 +0000

    done cover and realated_images

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -71,6 +71,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :descriptions)
+      params.require(:book).permit(:title, :descriptions, :cover, realated_images: [])
     end
 end

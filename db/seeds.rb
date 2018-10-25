1c2c94c98d9416a376a5b0791a922c9bddfc7968
tangvinhduong <me@yeuem1vannam.com>
2018-10-25 03:19:19 +0000

    Done fix cover and css edit and css new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.first
+images = Dir["db/seeds/*.jpg"]
+
+20.times do
+  book = Book.new(
+    user: user,
+    title: Faker::Book.title,
+    description: Faker::ChuckNorris.fact
+  )
+  image = images.sample             
+  book.cover.attach(io: File.open(image), filename: image, content_type: "image/jpg")
+  images.sample(2).each do |image|
+    book.related_images.attach(io: File.open(image), filename: image, content_type: "image/jpg")
+  end
+  book.save && puts("Created #{book.id} - #{book.title}")
+end
